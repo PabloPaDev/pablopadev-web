7f91e381c722ba9805031ab3948d42274c182edc
pablo palacios <pablopalavi@gmail.com>
2025-11-03 20:27:36 +0100

    chore(CardNav): remove 'Get Started' CTA

--- components/CardNav/CardNav.jsx
+++ components/CardNav/CardNav.jsx
@@ -174,13 +174,7 @@ const CardNav = ({
             <img src={logo} alt={logoAlt} className="logo h-[28px]" />
           </div>
 
-          <button
-            type="button"
-            className="card-nav-cta-button hidden md:inline-flex border-0 rounded-[calc(0.75rem-0.2rem)] px-4 items-center h-full font-medium cursor-pointer transition-colors duration-300"
-            style={{ backgroundColor: buttonBgColor, color: buttonTextColor }}
-          >
-            Get Started
-          </button>
+          {/* CTA eliminado */}
         </div>
 
         <div
